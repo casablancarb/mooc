827332ecb1488816cb9c2fcc1697aad44103de86
Christopher Okhravi <christopher.okhravi@gmail.com>
2013-08-27 21:15:04 +0200

    Logging out takes you to the log in page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -19,6 +19,6 @@ class SessionsController < ApplicationController
   def destroy
     session[:user_id] = nil
     flash[:success] = "Successfully logged out"
-    redirect_to :root
+    redirect_to login_path
   end
 end
