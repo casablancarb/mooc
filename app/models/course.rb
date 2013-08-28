67d423fad8da6b45130f480b231522b4694c04a4
Christopher Okhravi <christopher.okhravi@gmail.com>
2013-08-28 23:26:46 +0200

    Server-side validation for courses

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,5 +1,5 @@
 class Course < ActiveRecord::Base
   belongs_to :user
   validates_length_of :semester, in: 1..2
-  validates_presence_of :user
+  validates_presence_of :user, :title, :semester
 end
