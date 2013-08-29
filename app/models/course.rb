8638db9a2d3e3e6e901f4f692b553ddd2abda9a4
Christopher Okhravi <christopher.okhravi@gmail.com>
2013-08-29 10:01:55 +0200

    Year selection for courses, mandatory

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,7 +2,7 @@ class Course < ActiveRecord::Base
   belongs_to :user
   has_many :admissions
   validates_length_of :semester, in: 1..2
-  validates_presence_of :user, :title, :semester
+  validates_presence_of :user, :title, :semester, :year
 
   def owned_by? other_user
     user.id == other_user.id
