d620d605e82be97e4c90c866949b40f9ab62c34a
Christopher Okhravi <christopher.okhravi@gmail.com>
2013-08-29 08:54:35 +0200

    Migrations for admissions

---
+++ db/migrate/20130829064618_create_admissions.rb
@@ -0,0 +1,8 @@
+class CreateAdmissions < ActiveRecord::Migration
+  def change
+    create_table :admissions do |t|
+      t.integer :user_id
+      t.integer :course_id
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130828201517) do
+ActiveRecord::Schema.define(version: 20130829064618) do
+
+  create_table "admissions", force: true do |t|
+    t.integer "user_id"
+    t.integer "course_id"
+  end
 
   create_table "courses", force: true do |t|
     t.string  "title"
