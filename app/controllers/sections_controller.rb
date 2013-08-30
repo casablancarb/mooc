c30a1e6cd1239755f772e892ed1ec23b9fae4254
Christopher Okhravi <christopher.okhravi@gmail.com>
2013-08-30 16:08:44 +0200

    Breadcrumb for sections#show

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -1,5 +1,10 @@
 class SectionsController < ApplicationController
   def show
     @section = Section.find(params[:id]).decorate
+    @breadcrumbs = [
+      Breadcrumb.new('My studies', studies_path),
+      Breadcrumb.new(@section.course.title, course_path(@section.course)),
+      Breadcrumb.new(@section.title)
+    ]
   end
 end
