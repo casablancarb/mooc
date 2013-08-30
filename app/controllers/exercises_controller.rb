a88248535346fb72bcb50fb9d52010af7fa1a233
Christopher Okhravi <christopher.okhravi@gmail.com>
2013-08-30 16:22:05 +0200

    Breadcrumb

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -1,5 +1,12 @@
 class ExercisesController < ApplicationController
   def show
     @exercise = Exercise.find params[:id]
+
+    @breadcrumbs = [
+      Breadcrumb.new('My studies', studies_path),
+      Breadcrumb.new(@exercise.section.course.title, course_path(@exercise.section.course)),
+      Breadcrumb.new(@exercise.section.title, section_path(@exercise.section)),
+      Breadcrumb.new(@exercise.title)
+    ]
   end
 end
