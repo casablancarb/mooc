aacc257b1c6bc79db1626a87a9c72b5037f91bf5
Christopher Okhravi <christopher.okhravi@gmail.com>
2013-08-30 15:13:32 +0200

    inverse_of worked incorrectly due to me choosing *_id instead of *

--- app/models/alternative.rb
+++ app/models/alternative.rb
@@ -1,4 +1,4 @@
 class Alternative < ActiveRecord::Base
   belongs_to :question, inverse_of: :alternatives
-  validates_presence_of :body, :explanation, :question_id
+  validates_presence_of :body, :explanation, :question
 end
