732cb0d97e13c40d08d5804e1ee90c127be97356
Christopher Okhravi <christopher.okhravi@gmail.com>
2013-09-05 20:45:27 +0200

    Cucumber features for login

---
+++ features/login.feature
@@ -0,0 +1,20 @@
+Feature: Loggin in
+  As a user with an account
+  In order to use the system
+  I want to be able to log in
+
+  Scenario: Logging in with correct credentials
+    Given I have a user account
+    And I am on the home page
+    And I click the menu item "Log in"
+    And I enter my credentials
+    And I click the button "Log in"
+    Then I should see a successful flash saying "Successfully logged in"
+
+  Scenario: Logging in with incorrect credentials
+    Given I have a user account
+    And I am on the home page
+    And I click the menu item "Log in"
+    And I enter incorrect credentials
+    And I click the button "Log in"
+    Then I should see a error flash saying "Email or password is invalid"
---
+++ features/step_definitions/login.rb
@@ -0,0 +1,35 @@
+Given(/^I have a user account$/) do
+  @user = FactoryGirl.create :user
+end
+
+Given(/^I am on the home page$/) do
+  visit '/'
+end
+
+Given(/^I click the menu item "(.*?)"$/) do |link|
+  within(:css, '.navbar') do
+    click_link 'Log in'
+  end
+end
+
+Given(/^I enter my credentials$/) do
+  fill_in 'email', with: 'jane@example.com'
+  fill_in 'password', with: '123123'
+end
+
+Given(/^I click the button "(.*?)"$/) do |name|
+  click_button name
+end
+
+Then(/^I should see a successful flash saying "(.*?)"$/) do |message|
+  page.find(:css, '.alert-success').text.should include(message)
+end
+
+Given(/^I enter incorrect credentials$/) do
+  fill_in 'email', with: 'jane@example.com'
+  fill_in 'password', with: 'incorrect-password'
+end
+
+Then(/^I should see a error flash saying "(.*?)"$/) do |message|
+  page.find(:css, '.alert-error').text.should include(message)
+end
