59366fbbfdad25b328efa3cae64b28e5de5290a1
Christopher Okhravi <christopher.okhravi@gmail.com>
2013-09-05 15:12:53 +0200

    Factory not fulfilling model requirements of password length

--- spec/factories.rb
+++ spec/factories.rb
@@ -5,8 +5,8 @@ FactoryGirl.define do
     firstname 'Jane'
     lastname 'Doe'
     email 'jane@example.com'
-    password '123'
-    password_confirmation '123'
+    password '123123'
+    password_confirmation '123123'
   end
 
   factory :course do
