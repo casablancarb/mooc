0c0233b21070e828db2e3f611299abcd76563cac
Christopher Okhravi <christopher.okhravi@gmail.com>
2013-09-07 21:48:33 +0200

    Specs for making sure only the users own answers are considered when asking for correctness in questions

--- spec/unit/question_spec.rb
+++ spec/unit/question_spec.rb
@@ -4,10 +4,9 @@ describe Question do
   let(:question){ FactoryGirl.create :question }
   let(:user){ question.exercise.section.course.user }
   let(:alternative){ question.alternatives.first }
+  let(:another_user){ FactoryGirl.create :user }
 
   context "#answer_from_user" do
-    let(:another_user){ FactoryGirl.create :user }
-
     it "responds with the answer given by the user" do
       answer = FactoryGirl.create :answer, {
         alternative: alternative,
@@ -46,7 +45,22 @@ describe Question do
       question.is_correctly_answered_by_user?(user).should == false
     end
 
-    it "does not confuse thev value with another user"do
+    it "responds with false if the question is correctly answered by another user, but not this one" do
+      alternative.truth_value = true
+      alternative.save!
+      answer = FactoryGirl.create :answer, {
+        alternative: alternative,
+        user: FactoryGirl.create(:user)
+      }
+      question.is_correctly_answered_by_user?(user).should == false
+    end
+
+    it "responds with false if the question is incorrectly answered by another user, but correctly by this one" do
+      correct_alternative = FactoryGirl.create :correct_alternative, {question: question}
+      incorrect_alternative = FactoryGirl.create :incorrect_alternative, {question: question}
+      FactoryGirl.create :answer, { alternative: incorrect_alternative, user: another_user }
+      FactoryGirl.create :answer, { alternative: correct_alternative, user: user }
+      question.is_correctly_answered_by_user?(user).should == true
     end
   end
 end
