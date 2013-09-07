225eea1838daa5ff28f9b8af48f08503cef5d3b4
Christopher Okhravi <christopher.okhravi@gmail.com>
2013-09-07 21:27:41 +0200

    A new fly way to calculate progress

--- app/models/progress_calculator.rb
+++ app/models/progress_calculator.rb
@@ -13,17 +13,26 @@ class ProgressCalculator
 
   def self.calculate_progress_from_progress_collection_for_user(progress_collection, user)
     return 0 if progress_collection.length < 1
-    progress = 0
-    total = progress_collection.length * 100
-    progress_collection.each do |s|
-      progress += s.progress_for_user(user)
+    points_available_per_part = 100 / (progress_collection.length.to_f)
+    points = 0
+    progress_collection.each do |progress_item|
+      progress_for_item = progress_item.progress_for_user(user)
+      points_for_part = points_available_per_part * (progress_for_item.to_f / 100)
+      points += points_for_part
     end
-    (progress.to_f / total * 100).to_i
+    points.to_i
   end
 
   def self.calculate_progress_from_questions_for_user(questions, user)
     total = questions.length > 0 ? questions.length : 1
     correct = questions.select{ |q| q.is_correctly_answered_by_user?(user) }.length
+    (correct.to_f / total * 100).to_i
+  end
+
+  def self.for_user_in_exercise(user, exercise)
+    questions = exercise.number_of_questions
+    total  = questions > 0 ? questions : 1
+    correct = exercise.number_of_correctly_answered_questions_by_user(user)
     (correct.to_f / total * 100).to_i
   end
 end
--- spec/unit/progress_calculator_spec.rb
+++ spec/unit/progress_calculator_spec.rb
@@ -1,4 +1,15 @@
-require 'spec_helper'
+require File.expand_path('app/models/progress_calculator')
 
 describe ProgressCalculator do
+  context ".calculate_progress_from_progress_collection_for_user" do
+    it "calculates the progress" do
+      stubs = []
+      values = [100, 50, 50, 100]
+      values.each do |value|
+        stubs << double
+        stubs.last.stub(:progress_for_user){ value }
+      end
+      ProgressCalculator.calculate_progress_from_progress_collection_for_user(stubs, stub).should == 75
+    end
+  end
 end
