236e81fa76b34c82ab95b708cbeb2bdd6af1a114
Christopher Okhravi <christopher.okhravi@gmail.com>
2013-09-20 15:40:02 +0200

    Specs for user_takes_course?

--- spec/unit/user_spec.rb
+++ spec/unit/user_spec.rb
@@ -12,4 +12,18 @@ describe User do
       subject.is_teacher?.should be_false
     end
   end
+
+  context "takes_course?" do
+    let(:course){ FactoryGirl.create :course }
+    let(:user){ FactoryGirl.create :user }
+
+    it "returns true if the user is admitted to the course" do
+      FactoryGirl.create :admission, user:user, course:course
+      user.takes_course?(course).should be_true
+    end
+
+    it "returns false if the user is not admitted to the course" do
+      user.takes_course?(course).should be_false
+    end
+  end
 end
