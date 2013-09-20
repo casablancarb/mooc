caacc6217bda4ad40f9d5857693be3db20514405
Christopher Okhravi <christopher.okhravi@gmail.com>
2013-09-20 19:29:26 +0200

    Enforcing read access for exercises

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -3,11 +3,15 @@ class ExercisesController < ApplicationController
     @exercise = Exercise.find_published(params[:id]).decorate
     @comment = Comment.new
 
-    @breadcrumbs = [
-      Breadcrumb.new('My studies', studies_path),
-      Breadcrumb.new(@exercise.section.course.title, course_path(@exercise.section.course)),
-      Breadcrumb.new(@exercise.section.title, section_path(@exercise.section)),
-      Breadcrumb.new(@exercise.title)
-    ]
+    if ExercisePolicy.user_can_read?(current_user, @exercise)
+      @breadcrumbs = [
+        Breadcrumb.new('My studies', studies_path),
+        Breadcrumb.new(@exercise.section.course.title, course_path(@exercise.section.course)),
+        Breadcrumb.new(@exercise.section.title, section_path(@exercise.section)),
+        Breadcrumb.new(@exercise.title)
+      ]
+    else
+      redirect_to :root, status: 401
+    end
   end
 end
--- spec/controllers/exercises_controller_spec.rb
+++ spec/controllers/exercises_controller_spec.rb
@@ -7,14 +7,36 @@ describe ExercisesController do
       controller.stub(:authorize){ true }
     end
 
-    it "sets the instance variable exercise" do
-      get :show, id:@exercise.id
-      assigns(:exercise).should == @exercise
+    context "when the user can read the course" do
+      before :each do
+        ExercisePolicy.stub(:user_can_read?){ true }
+      end
+
+      it "sets the instance variable exercise" do
+        get :show, id:@exercise.id
+        assigns(:exercise).should == @exercise
+      end
+
+      it "sets the instance variable comment" do
+        get :show, id:@exercise.id
+        assigns(:comment).should be_kind_of(Comment)
+      end
     end
 
-    it "sets the instance variable comment" do
-      get :show, id:@exercise.id
-      assigns(:comment).should be_kind_of(Comment)
+    context "when the user does not have access" do
+      let(:subject){ get :show, id:@exercise.id }
+
+      before :each do
+        ExercisePolicy.stub(:user_can_read?){ false }
+      end
+
+      it "returns status unauthorized" do
+        subject.status.should == 401
+      end
+
+      it "does not render exercise" do
+        subject.should_not render_template(:show)
+      end
     end
   end
 end
